86aa33e0285025bdcf932b49c483d58453aaf944
BellaAguirre <yunoshe1@gmail.com>
2018-08-16 15:55:00 -0500

    close #5

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ fs.lstat (route, (err, stats) => {
     console.log('es file');
     if (path.extname(route) === '.md') {
       console.log('es md');
+      fs.readFile(route,'utf8',(err, data) => {
+        console.log(data);
+      });
     }
+  } else {
+    console.log('es directorio')
   }
 })
