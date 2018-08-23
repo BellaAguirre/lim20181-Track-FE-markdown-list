18aa45aea8c66c267bb3312c688fbd734bccb895
BellaAguirre <yunoshe1@gmail.com>
2018-08-23 03:03:06 -0500

    demo filtrando archivos de una carpeta

--- src/index.js
+++ src/index.js
@@ -5,89 +5,126 @@ const fetch = require('node-fetch');
 const async = require("async");
 const [,, ...args] = process.argv;
 const route = args[0];
-// statusUrl(arr).then((respose)=>{})
-const linksStatus = (arr) => {
-  // console.log(arr);
-  
-}
-const statusUrl = (objet) => {
-  // return new Promise
-let arr = [];
-objet.map(obje => {
-  fetch(obje.url)
-  .then((response) => {
-    const obj = {
-      url: obje.url,
-      text: obje.text,
-      file: obje.file,
-      status: response.status
-    }
-    arr.push(obj);
-    if (objet.length === arr.length)
-    // console.log(arr)
-    linksStatus(arr);
-    return (arr);
-  })
-  .catch((err) => {
-    const obj = {
-      url: obje.url,
-      text: obje.text,
-      file: obje.file,
-      status: 'fail'
-    }
-      arr.push(obj);
-  })
-})
-  
+
+let options = {
+  validate : false,
+  stats: false,
 }
 
-const readFileUrl = (routeFile) => {
-  const arrayUrl = [];
-  if (path.extname(routeFile) === '.md') {
-    console.log('es md');
-    fs.readFile(routeFile,'utf8',(err, data) => {
-      if (err) throw err;
+const mdLinks = (route, options, arr) => {
+  if (options.validate) {
+    arr.forEach(link => {
+      console.log(link.file + ' ' + link.url + ' ' + link.status )
+    })
+    options.validate = false;
+  } else if (options.stats) {
+    let count = 0;
+    // console.log(arr.length + 'total ' + count  + 'unicos ') 
+  }
+}
+
+const mdObjectLinks = (arr) => {
+  // console.log(arr);
+  // console.log(args[1]);
+  
+  if (args[1] === '--validate') {
+    options.validate = true;
+    mdLinks(route, options, arr)
+  } else if (args[1] === '--stats') {
+    
+    options.stats = true;
+    mdLinks(route, options, arr)
+  }
+  
+}
+const statusLink = (arrayLinks) => {
+  // console.log(arrayLinks);
+  let arr = [];  
+  arrayLinks.forEach(link => {
+  fetch(link.url)
+    .then((response) => {
+      const obj = {
+        url: link.url,
+        text: link.text,
+        file: link.file,
+        status: response.status + ' ok'
+        // ok: 'ok'
+      }
+      arr.push(obj);
+      if (arr.length === arrayLinks.length) {
+        // console.log(arr);
+        mdObjectLinks(arr);
+      }
+      return obj;
+    })
+    .catch((err) => {
+      const obj = {
+        url: link.url,
+        text: link.text,
+        file: link.file,
+        status: 'fail'
+      }
+      arr.push(obj);
+      return obj;
+    })
+  })
+}
+
+const readFile = (arrayFile) => {
+  const arrayLink = [];
+  for (const file of arrayFile) {
+    if (path.extname(file) === '.md') {
+      const read = fs.readFileSync(file,'utf8');
       const exp = /\[(.*?)\]\(.*?\)/gm;
-      const dataFile = data.match(exp);
-      if(dataFile !== null) {
-        dataFile.forEach(ele => {
-          const final = ele.indexOf(']');
-          const obj = {
-            url: ele.slice(final + 2, ele.length - 1),
-            text: ele.slice(1, final),
-            file: path.resolve(routeFile)
-          }
-          arrayUrl.push(obj);
-        });
-       }
-      // statusUrl(arrayUrl);
-    });
-  }
-}
-const readDir = (route) => {
-  fs.readdir(route, (err, files) => {
-    if (err) throw err;
-    for (const item of files) {
-      fs.stat ((route+'/'+item),(err, stats) => {
-        if(stats.isFile()) {
-          console.log('es file')
-          readFileUrl(route+'/'+item);
-        } else if(stats.isDirectory()) {
-          console.log('es directorio')
-          readDir(route+'/'+item);
+      const dataFile = read.match(exp);
+      dataFile.forEach(ele => {
+        const initial = ele.indexOf('[');
+        const final = ele.indexOf(']');
+        const obj = {
+          url:ele.slice(final + 2, ele.length - 1),
+          text: ele.slice(1, final),
+          file: path.resolve(file)
         }
-      })
+        arrayLink.push(obj);
+      });
     }
-  });
-}
-fs.stat (route, (err, stats) => {
-  if (err) {
-    console.log(err);
-  } else if (stats.isFile()) {
-    console.log('es file');
-    readFileUrl(route);
-  } else {
-    console.log('es directorio');
-    readDir(route)
   }
-})
+  return arrayLink;
+}
+
+const readDir = (routeRead) => {
+  let arr = [];
+  const files = fs.readdirSync(routeRead, 'utf8');
+  for (const file of files) {
+    const stat = fs.statSync(routeRead + '/' + file);
+    if (stat.isFile()) {
+      arr.push(routeRead + '/' + file);
+    } 
+    if (stat.isDirectory()) {
+      arr = arr.concat(readDir(routeRead + '/' + file));
+    }
+  }
+  return arr
+}
+
+
+try {
+  const arrayFile = [];
+  const stat = fs.statSync(route);
+  if (stat.isFile()) {
+    arrayFile.push(route);
+    const links = readFile(arrayFile);
+    statusLink(links);
+  } 
+  if (stat.isDirectory()) {
+    const files = readDir(route);
+    const links = readFile(files);
+    const aa = statusLink(links);
+    console.log(aa);
+    
+  }
+
+} catch (err) {
+  throw err.path + 'esta ruta no existe';
+}
+
