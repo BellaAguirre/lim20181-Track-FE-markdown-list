00dd9a7aec9c3a50e52ba2066fa645f83f806b4d
BellaAguirre <yunoshe1@gmail.com>
2018-08-28 20:38:06 -0500

    closes #19

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
-const mdLinks = require('./index.js')
+const mdLinks = require('./index.js');
 const [,, ...args] = process.argv
 const route = args[0]
 
-let options = {
-  validate : false,
+const options = {
+  validate: false,
   stats: false,
-}
+};
 
+if (args.length === 0) {
+  console.log('ingresa un ruta ');
+}
 if (args[0] && args[1] === undefined) {
   options.validate = false;
   options.stats = false;
   mdLinks(route, options).then(response => {
-    console.log(response);
+    response.forEach(element => {
+      console.log(element.file + ' ' + element.url + ' ' + element.text);
+    });
   });
 } else if (args[0] && args[1] === '--validate' && args[2] === '--stats') {
   options.validate = true;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const fs = require('fs')
-const path = require('path')
-const fetch = require('node-fetch')
-const async = require('async')
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+const async = require('async');
 
 const validateLink = (obj, callback) => {
   fetch(obj.url)
@@ -10,12 +10,6 @@ const validateLink = (obj, callback) => {
         callback(null, {
           ...obj,
           valido: true,
-          status: response.status,
-        });
-      } else {
-        callback(null, {
-          ...obj,
-          valido: false,
           status: response.status,
         });
       }
@@ -27,105 +21,128 @@ const validateLink = (obj, callback) => {
         status: 404,
       });
     });
-}
+};
 
 const statusLink = (arrayLinks) => {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     async.map(arrayLinks, validateLink, (err, results) => {
       if (err) reject(err);
       resolve(results);
-    })
-  })
-}
+    });
+  });
+  return promise;
+};
 
 const readFileMd = (arrayFile) => {
   const arrayLink = [];
-  for (const file of arrayFile) {
-    if (path.extname(file) === '.md') {
+  const fileMd = arrayFile.filter(file => path.extname(file) === '.md');
+  if (fileMd.length !== 0) {
+    fileMd.forEach((file) => {
       const read = fs.readFileSync(file, 'utf8');
       const exp = /\[(.*?)\]\(.*?\)/gm;
       const dataFile = read.match(exp);
-      dataFile.forEach(ele => {
-        const final = ele.indexOf(']');
-        const obj = {
-          url: ele.slice(final + 2, ele.length - 1),
-          text: ele.slice(1, final),
-          file: path.resolve(file)
-        }
-        arrayLink.push(obj);
-      });
-    }
+      if (dataFile !== null) {
+        dataFile.forEach((ele) => {
+          const final = ele.indexOf(']');
+          const obj = {
+            url: ele.slice(final + 2, ele.length - 1),
+            text: ele.slice(1, final),
+            file: path.resolve(file),
+          };
+          arrayLink.push(obj);
+        });
+      }
+    });
+    return arrayLink;
   }
-  return arrayLink;
-}
+};
 
 const statPath = (route) => {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     fs.stat(route, (err, stats) => {
-      if (err) reject(err);
-      resolve(stats.isFile());
+      try {
+        resolve(stats.isFile());
+      } catch (e) {
+        reject(err);
+      }
     });
   });
-}
+  return promise;
+};
 
 const readDir = (route) => {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     fs.readdir(route, (err, files) => {
-      if (err) reject(err)
+      if (err) reject(err);
       resolve(files);
-    })
+    });
   });
-}
+  return promise;
+};
 
 const flatten = (arrayFile) => {
-  return arrayFile.reduce((flat, toFlatten) => {
-    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  const newArray = arrayFile.reduce((flat, toFlatten) => {
+    const flatArray = flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    return flatArray;
   }, []);
-}
+  return newArray;
+};
 
 const dirOrFile = (route) => {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     try {
       resolve(
         statPath(route)
-          .then(stat => {
+          .then((stat) => {
             if (stat) return [route];
             return readDir(route)
-              .then(files => files.map(file => dirOrFile(route + '/' + file)))
+              .then(files => files.map(file => dirOrFile(path.join(route, file))))
               .then(promises => Promise.all(promises))
-              .then(arr => flatten(arr))
-          })
+              .then(arr => flatten(arr));
+          }),
       );
     } catch (err) {
       reject(err);
     }
   });
-}
+  return promise;
+};
 
 const mdLinks = (route, options) => {
-  let broquen = 0;
+  let broque = 0;
   return new Promise((resolve, reject) => {
-    if (options.validate && options.stats)
-      dirOrFile(route)
-      .then(response => readFileMd(response))
-      .then(links => statusLink(links))
-      .then(response => {
-        const linkUnique = response.map(item => item.url).filter((value, index, self) => self.indexOf(value) === index);
-        response.forEach(link => {
-          if (!link.valido) broquen++;
-        });
-        resolve('total: ' + response.length + ' unique: ' + linkUnique.length + ' broquen: ' + broquen);
-      });
-    else if (options.validate) dirOrFile(route).then((response) => readFileMd(response)).then(status => resolve(statusLink(status)))
-    else if (options.stats)
-    dirOrFile(route)
-    .then(response => readFileMd(response))
-    .then(response => {
-      const linkUnique = response.map(item => item.url).filter((value, index, self) => self.indexOf(value) === index);
-      resolve('total: ' + response.length + ' unique: ' + linkUnique.length);
-    });
-    else dirOrFile(route).then(response => resolve(readFileMd(response)));
+    try {
+      if (options.validate && options.stats) {
+        dirOrFile(route)
+          .then(response => readFileMd(response))
+          .then(links => statusLink(links))
+          .then((response) => {
+            const linkUnique = response.map(item => item.url)
+              .filter((value, index, self) => self.indexOf(value) === index);
+            response.forEach((link) => {
+              if (!link.valido) {
+                broque = 1 + broque;
+              }
+            });
+            resolve({ total: response.length, unique: linkUnique.length, broquen: broque });
+          });
+      } else if (options.validate) {
+        dirOrFile(route)
+          .then(response => readFileMd(response))
+          .then(status => resolve(statusLink(status)));
+      } else if (options.stats) {
+        dirOrFile(route)
+          .then(response => readFileMd(response))
+          .then((response) => {
+            const linkUnique = response.map(item => item.url)
+              .filter((value, index, self) => self.indexOf(value) === index);
+            resolve('total: ', response.length, ' unique: ', linkUnique.length);
+          });
+      } else dirOrFile(route).then(response => resolve(readFileMd(response)));
+    } catch (err) {
+      reject(err.message);
+    }
   });
-}
+};
 
-module.exports = mdLinks
+module.exports = mdLinks;
